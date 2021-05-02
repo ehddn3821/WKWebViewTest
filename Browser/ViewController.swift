e703761308a783c61e7a32bfe7fa045c5cb4d360
dwkang <ehddn3821@naver.com>
2021-05-02 17:56:55 +0900

    #2 WebView에 테스트 페이지 띄우기

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -10,16 +10,50 @@ import WebKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet var urlField: UITextField!
     @IBOutlet var progressBar: UIProgressView!
     @IBOutlet var mainWebView: WKWebView!
     
+    
+    // MARK: - Parameters
+    var urlStr = "https://www.apple.com"
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        urlField.delegate = self
         
+        mainWebView.navigationDelegate = self
+        mainWebView.loadUrlString(urlString: urlStr)
     }
 
 
 }
 
+
+//MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    
+    
+}
+
+
+// MARK: - WKNavigationDelegate
+extension ViewController: WKNavigationDelegate {
+    
+}
+
+
+// MARK: - WKWebView Extension
+extension WKWebView {
+    func loadUrlString(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
