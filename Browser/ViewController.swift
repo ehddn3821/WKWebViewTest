fca14679160d250e703b04096e3a7eb8f726c1ee
dwkang <ehddn3821@naver.com>
2021-05-02 17:16:28 +0900

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by dwKang on 2021/05/02.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var urlField: UITextField!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var mainWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
